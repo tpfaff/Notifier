831fdfd2e2207ba4b81fae05779b78069418cd05
unknown <tylerpfaff@gmail.com>
2014-01-11 18:06:48 -0800

    Split notification layout into relative layout

--- gen/com/example/notifier/R.java
+++ gen/com/example/notifier/R.java
@@ -22,20 +22,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int notification_template=0x7f020001;
+        public static final int notification_img_background=0x7f020001;
+        public static final int notification_template=0x7f020002;
+        public static final int notification_text_background=0x7f020003;
     }
     public static final class id {
-        public static final int action_bar_menu=0x7f080007;
-        public static final int action_settings=0x7f08000a;
-        public static final int add_notification=0x7f080008;
-        public static final int delete_notification=0x7f080009;
+        public static final int action_bar_menu=0x7f080009;
+        public static final int action_settings=0x7f08000c;
+        public static final int add_notification=0x7f08000a;
+        public static final int delete_notification=0x7f08000b;
         public static final int edit_header_text=0x7f080001;
         public static final int edit_subtext=0x7f080002;
         public static final int edit_template_layout=0x7f080004;
         public static final int enableNotificationSwitch=0x7f080003;
-        public static final int lblListHeader=0x7f080005;
-        public static final int lblListItem=0x7f080006;
+        public static final int lblListHeader=0x7f080007;
+        public static final int lblListItem=0x7f080008;
         public static final int list=0x7f080000;
+        public static final int notification_img_view=0x7f080005;
+        public static final int text_enter_layout=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
